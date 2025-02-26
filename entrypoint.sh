34d1194860adc8842b898558bac460528df4c9d4
hojin22 <hojin22@mobigen.com>
2025-02-26 15:49:14 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,15 @@
 if [ ! "$(ls -A /var/lib/pgsql/data)" ]; then
   /usr/pgsql-14/bin/initdb
   sudo cp /defaultfile/*.conf /var/lib/pgsql/14/data
-  /usr/pgsql-14/bin/pg_ctl -D /var/lib/pgsql/14/data -l logfile start &
-  sleep 30
-  psql -U postgres -h localhost -p 5432 -c "CREATE USER data_catalog PASSWORD 'data_catalog123' SUPERUSER; "
-  psql -U postgres -h localhost -p 5432 -c "CREATE DATABASE data_catalog;"
-  psql -U postgres -h localhost -p 5432 -d data_catalog -f /defaultfile/data_catalog_init.sql
+  sudo chown postgres:postgres /var/lib/pgsql/14/data/*.conf
+#  /usr/pgsql-14/bin/pg_ctl -D /var/lib/pgsql/14/data -l logfile start &
+#  sleep 30
+#  psql -U postgres -h localhost -p 5432 -c "CREATE USER data_catalog PASSWORD 'data_catalog123' SUPERUSER; "
+#  psql -U postgres -h localhost -p 5432 -c "CREATE DATABASE data_catalog;"
+#  psql -U postgres -h localhost -p 5432 -d data_catalog -f /defaultfile/data_catalog_init.sql
 else
   /usr/pgsql-14/bin/pg_ctl -D /var/lib/pgsql/14/data -l logfile start &
 fi
 
+tail -f /dev/null
 wait
