489b357f0549b74ba565160ba0a895c9514d6c5f
hojin22 <hojin22@mobigen.com>
2025-02-26 14:58:03 +0900

    초기구축

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-if [ ! -d "/elasticsearch-8.9.0/data" ]; then
-  sudo mkdir /elasticsearch-8.9.0/data
+#RUN
+
+if [ ! "$(ls -A /var/lib/pgsql/data)" ]; then
+  /usr/pgsql-14/bin/initdb
+  #cp -r /defaultfile/elasticsearch-8.9.0/config/* /var/lib/pgsql/data
 fi
 
-if [ ! -d "/elasticsearch-8.9.0/config/analysis" ]; then
-  sudo mkdir /elasticsearch-8.9.0/config/analysis
-fi
-
-if [ ! "$(ls -A /elasticsearch-8.9.0/config)" ]; then
-  sudo cp -r /defaultfile/elasticsearch-8.9.0/config/* /elasticsearch-8.9.0/config
-fi
-
-sudo chown -R elastic:elastic /elasticsearch-8.9.0
-
-sudo touch /elasticsearch-8.9.0/config/analysis/userdic.txt
-sudo touch /elasticsearch-8.9.0/config/analysis/synonym.txt
-
-cd /elasticsearch-8.9.0
-/elasticsearch-8.9.0/bin/elasticsearch
+#/usr/pgsql-14/bin/pg_ctl -D /var/lib/pgsql/data -l logfile start
+tail -f /dev/null
