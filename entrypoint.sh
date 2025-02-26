c0e6750b38b7e2119dc815604fbfe96f97d1371b
hojin22 <hojin22@mobigen.com>
2025-02-26 15:59:08 +0900

    적재 코드 수정

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,13 @@ if [ ! "$(ls -A /var/lib/pgsql/data)" ]; then
   /usr/pgsql-14/bin/initdb
   sudo cp /defaultfile/*.conf /var/lib/pgsql/14/data
   sudo chown -R postgres:postgres /var/lib/pgsql/14/data
-  /usr/pgsql-14/bin/pg_ctl -D /var/lib/pgsql/14/data -l logfile start &
-#  sleep 30
-#  psql -U postgres -h localhost -p 5432 -c "CREATE USER data_catalog PASSWORD 'data_catalog123' SUPERUSER; "
-#  psql -U postgres -h localhost -p 5432 -c "CREATE DATABASE data_catalog;"
-#  psql -U postgres -h localhost -p 5432 -d data_catalog -f /defaultfile/data_catalog_init.sql
+  /usr/pgsql-14/bin/pg_ctl -D /var/lib/pgsql/14/data start &
+  sleep 30
+  psql -U postgres -p 5432 -c "CREATE USER data_catalog PASSWORD 'data_catalog123' SUPERUSER; "
+  psql -U postgres -p 5432 -c "CREATE DATABASE data_catalog;"
+  psql -U postgres -p 5432 -d data_catalog -f /defaultfile/data_catalog_init.sql
 else
-  /usr/pgsql-14/bin/pg_ctl -D /var/lib/pgsql/14/data -l logfile start &
+  /usr/pgsql-14/bin/pg_ctl -D /var/lib/pgsql/14/data start &
 fi
 
-tail -f /dev/null
 wait
